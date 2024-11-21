b1e68502b4a7bb44635e4d8256bd57a5d7badeb7
Ninjatosba <adenizcorlu@gmail.com>
2024-11-21 13:14:47 +0300

    init

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use dutch_auction_launchpad::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
